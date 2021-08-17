f16bf8d22d42e770b2d9cec31e264f0c01b2612a
sally20921 <sally20921@github.com>
2021-08-17 18:50:29 -0400

    adding

--- test.py
+++ test.py
@@ -4,14 +4,43 @@ from torchvision import transforms, models, datasets
 from torch_snippets import Report
 from torchvision.ops import nms
 from cv2 import cv2
+import torch.utils.data as data
+import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 IMAGE_ROOT = 'images/images'
 DF_RAW = pd.read_csv('df.csv')
+print(DF_RAW.head())
+
+class OpenImages(data.Dataset):
+    def __init__(self, df, image_folder=IMAGE_ROOT):
+        self.root = image_folder
+        self.df = df
+        self.unique_images = df['ImageID'].unique()
+
+    def __len__(self):
+        return len(self.unique_images)
+
+    def __getitem__(self, ix):
+        image_id = self.unique_images[ix]
+        image_path = f'{self.root}/{image_id}.jpg'
+        image = cv2.imread(image_path, 1)[...,::-1] # BGR to RGB
+        h, w, _ = image.shape
+        df = self.df.copy()
+        df = df[df['ImageID'] == image_id]
+        boxes = df['XMin,YMin,XMax,YMax'.split(',')].values
+        boxes = (boxes * np.array([w,h,w,h])).astype(np.uint16).tolist()
+        classes = df['LabelName'].values.tolist()
+        return image, boxes, classes, image_path
 
 
 
+
+def test():
+    ds = OpenImages(df=DF_RAW)
+    im, bbs, clss, _ = ds[9]
+    show(im, bbs=bbs, texts=clss, sz=10)
 
 if __name__=="__main__":
     test()
--- utils.py
+++ utils.py
@@ -3,4 +3,8 @@ import selectivesearch
 from torchvision import transforms, models, datasets
 from torch_snippets import Report
 from torchvision.ops import nms
+import numpy as np
+from cv2 import cv2
+
+def extract_candidates(img):
 
